3863d0dfd7e957e0f5ca6989a88215a33c17f8cb
Rocio Hinojosa <rhinojosa@cide.net>
2023-11-13 19:35:00 +0100

    fix

--- Feature1/main.go
+++ Feature1/main.go
@@ -4,4 +4,5 @@ func main() {
 	print("Esto es una feature 1")
 	print("Esto es un cambio en la feature una vez ya mergeada en development")
 	print("Esto es un segundo cambio en la feature una vez ya mergeada en development con squash")
+	print("Esto es un segundo cambio en la feature una vez ya se ha mergeado la Feature 3 en main")
 }
